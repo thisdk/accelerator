2bda0c066d80b769a6c8f96a667f340e3ebaefef
Qixiong Yuan <thisdk@gmail.com>
2024-02-10 23:13:03 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ $UDP2RAW_PORT ];then
     sed -i "s#udp2raw_port#$UDP2RAW_PORT#g" /etc/supervisor/conf.d/supervisord.conf
 else
-    sed -i "s#udp2raw_port#8586#g" /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s#udp2raw_port#8686#g" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 exec "$@"
