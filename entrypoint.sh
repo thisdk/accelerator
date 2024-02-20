29cce8f738d2c6bb6f8d94892caf95f6a9d3f377
Qixiong Yuan <thisdk@gmail.com>
2024-02-20 11:59:58 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 
+sleep 2
+
 if [ $UDP2RAW_PORT ];then
     sed -i "s#udp2raw_port#$UDP2RAW_PORT#g" /etc/supervisor/conf.d/supervisord.conf
 else
     sed -i "s#udp2raw_port#8585#g" /etc/supervisor/conf.d/supervisord.conf
 fi
+
+WIREGUARD_IP=$(dig +short wireguard)
+
+sed -i "s#wireguard#$WIREGUARD_IP#g" /etc/supervisor/conf.d/supervisord.conf
 
 exec "$@"
