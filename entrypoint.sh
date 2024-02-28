140b5911c4a6f3d1f5114683d8384422d23bb3a1
Qixiong Yuan <thisdk@gmail.com>
2024-02-28 09:36:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ cp -f /etc/supervisor/conf.d/supervisord.conf.backup /etc/supervisor/conf.d/supe
 
 WIREGUARD_IP=$(dig +short wireguard)
 
-sed -i "s#wireguard#$WIREGUARD_IP#g" /etc/supervisor/conf.d/supervisord.conf
-
 if [ $UDP_FEC ];then
     sed -i "s#udp_fec#$UDP_FEC#g" /etc/supervisor/conf.d/supervisord.conf
 else
@@ -18,6 +16,12 @@ if [ $UDP2RAW_PORT ];then
     sed -i "s#udp2raw_port#$UDP2RAW_PORT#g" /etc/supervisor/conf.d/supervisord.conf
 else
     sed -i "s#udp2raw_port#8585#g" /etc/supervisor/conf.d/supervisord.conf
+fi
+
+if [ $TARGET_ADDR ];then
+    sed -i "s#wireguard#$TARGET_ADDR#g" /etc/supervisor/conf.d/supervisord.conf
+else
+    sed -i "s#wireguard#$WIREGUARD_IP#g" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 exec "$@"
