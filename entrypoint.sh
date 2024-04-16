afa02fd6741b238327ca5c5b450478c397380255
Qixiong Yuan <thisdk@gmail.com>
2024-04-16 23:43:23 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,12 @@ else
     sed -i "s#UDP_FEC#2:6#g" /etc/supervisor/conf.d/supervisord.conf
 fi
 
+if [ $LISTENER_ADDR ];then
+    sed -i "s#LISTENER_ADDR#$LISTENER_ADDR#g" /etc/supervisor/conf.d/supervisord.conf
+else
+    sed -i "s#LISTENER_ADDR#$0.0.0.0#g" /etc/supervisor/conf.d/supervisord.conf
+fi
+
 if [ $TARGET_ADDR ];then
     sed -i "s#TARGET_ADDR#$TARGET_ADDR#g" /etc/supervisor/conf.d/supervisord.conf
 else
