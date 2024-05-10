dc89e4e2188fe211d728b656a96665abdd94d625
Qixiong Yuan <thisdk@gmail.com>
2024-05-11 00:40:08 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,10 @@ else
     sed -i "s#LISTEN_ADDR#0.0.0.0#g" /etc/supervisor/conf.d/supervisord.conf
 fi
 
+if [ $TARGET_ADDR ];then
+    sed -i "s#TARGET_ADDR#$TARGET_ADDR#g" /etc/supervisor/conf.d/supervisord.conf
+else
+    sed -i "s#TARGET_ADDR#sing-box:8585#g" /etc/supervisor/conf.d/supervisord.conf
+fi
+
 exec "$@"
