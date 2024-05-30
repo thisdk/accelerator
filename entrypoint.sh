27efd8a939fd57a6f2f9450887b831acb07eaf2d
Qixiong Yuan <thisdk@gmail.com>
2024-05-30 22:42:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,6 @@ sleep 3
 
 TARGET_IP=$(dig +short sing-box)
 
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
 iptables -t nat -A PREROUTING -p tcp --dport 8585 -j DNAT --to-destination $TARGET_IP:8585
 
 iptables -t nat -A POSTROUTING -d $TARGET_IP -p tcp --dport 8585 -j MASQUERADE
