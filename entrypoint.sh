061acf3a97fa7a24603f347ad627bc924b5adbee
Qixiong Yuan <thisdk@gmail.com>
2024-05-30 22:37:26 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
+sleep 3
+
+TARGET_IP=$(dig +short sing-box)
+
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-iptables -t nat -A PREROUTING -p tcp --dport 8585 -j DNAT --to-destination sing-box:8585
+iptables -t nat -A PREROUTING -p tcp --dport 8585 -j DNAT --to-destination $TARGET_IP:8585
 
-iptables -t nat -A POSTROUTING -p tcp --dport 8585 -j MASQUERADE
+iptables -t nat -A POSTROUTING -d $TARGET_IP -p tcp --dport 8585 -j MASQUERADE
 
 exec "$@"
