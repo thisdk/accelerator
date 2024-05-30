653a253cf16b3a6822564d970710240f600f9e05
Qixiong Yuan <thisdk@gmail.com>
2024-05-30 22:32:54 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+echo 1 > /proc/sys/net/ipv4/ip_forward
+
 iptables -t nat -A PREROUTING -p tcp --dport 8585 -j DNAT --to-destination sing-box:8585
 
 iptables -t nat -A POSTROUTING -p tcp --dport 8585 -j MASQUERADE
