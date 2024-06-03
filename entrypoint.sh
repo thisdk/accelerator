e0c8806bbc25cf819e82f6112ae167b1bf6c5e63
Qixiong Yuan <thisdk@gmail.com>
2024-06-03 10:07:31 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,13 @@ cp -f /etc/supervisor/conf.d/supervisord.conf.backup /etc/supervisor/conf.d/supe
 
 : ${TCN:=sing-box}
 
+: ${TCP:=8585}
+
 TARGET_IP=$(dig +short $TCN)
 
-iptables -t nat -A PREROUTING -p tcp --dport 8585 -j DNAT --to-destination $TARGET_IP:8585
+iptables -t nat -A PREROUTING -p tcp --dport $TCP -j DNAT --to-destination $TARGET_IP:$TCP
 
-iptables -t nat -A POSTROUTING -d $TARGET_IP -p tcp --dport 8585 -j MASQUERADE
+iptables -t nat -A POSTROUTING -d $TARGET_IP -p tcp --dport $TCP -j MASQUERADE
 
 if [ $UDP_FEC ]; then
     sed -i "s#UDP_FEC#$UDP_FEC#g" /etc/supervisor/conf.d/supervisord.conf
