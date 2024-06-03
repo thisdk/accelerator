d3dbbfe3771e4883d242e92b1402a20b0bc20bc6
Qixiong Yuan <thisdk@gmail.com>
2024-06-03 10:10:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ iptables -t nat -A POSTROUTING -d $TARGET_IP -p tcp --dport $TCP -j MASQUERADE
 if [ $UDP_FEC ]; then
     sed -i "s#UDP_FEC#$UDP_FEC#g" /etc/supervisor/conf.d/supervisord.conf
 else
-    sed -i "s#UDP_FEC#1:1,2:2,8:6,20:10#g" /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s#UDP_FEC#1:3,2:4,8:6,20:10#g" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 exec "$@"
