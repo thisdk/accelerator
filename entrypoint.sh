9cb216c2a1c53458dffdacbbb4acbc73a7bc462f
Qixiong Yuan <thisdk@gmail.com>
2024-07-20 21:40:10 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 sleep 3
 
+: ${LISTENER:=0.0.0.0}
 : ${TCN:=sing-box}
 : ${TCP:=8388}
 : ${FEC:=1:1,2:2,8:6,20:10}
@@ -12,6 +13,8 @@ iptables -t nat -A PREROUTING -p tcp --dport $TCP -j DNAT --to-destination $TARG
 iptables -t nat -A POSTROUTING -d $TARGET_IP -p tcp --dport $TCP -j MASQUERADE
 
 cp -f /etc/supervisor/conf.d/supervisord.conf.backup /etc/supervisor/conf.d/supervisord.conf
+
+sed -i "s#LISTENER#$LISTENER#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#FEC#$FEC#g" /etc/supervisor/conf.d/supervisord.conf
 
 exec "$@"
