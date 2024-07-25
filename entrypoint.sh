3fcaeab2465478bfaba09c5a890e6ff82d2a86e3
Qixiong Yuan <thisdk@gmail.com>
2024-07-25 11:24:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,19 +2,21 @@
 
 sleep 3
 
-: ${LISTENER:=0.0.0.0}
+: ${IPV:=0.0.0.0}
 : ${TCN:=sing-box}
 : ${TCP:=8388}
 : ${FEC:=1:1,2:2,8:6,20:10}
 
-TARGET_IP=$(dig +short $TCN)
+TIP=$(dig +short $TCN)
 
-iptables -t nat -A PREROUTING -p tcp --dport $TCP -j DNAT --to-destination $TARGET_IP:$TCP
-iptables -t nat -A POSTROUTING -d $TARGET_IP -p tcp --dport $TCP -j MASQUERADE
+iptables -t nat -A PREROUTING -p tcp --dport $TCP -j DNAT --to-destination $TIP:$TCP
+iptables -t nat -A PREROUTING -p udp --dport $TCP -j DNAT --to-destination $TIP:$TCP
+iptables -t nat -A POSTROUTING -d $TIP -p tcp --dport $TCP -j MASQUERADE
+iptables -t nat -A POSTROUTING -d $TIP -p udp --dport $TCP -j MASQUERADE
 
 cp -f /etc/supervisor/conf.d/supervisord.conf.backup /etc/supervisor/conf.d/supervisord.conf
 
-sed -i "s#LISTENER#$LISTENER#g" /etc/supervisor/conf.d/supervisord.conf
+sed -i "s#IPV#$IPV#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#FEC#$FEC#g" /etc/supervisor/conf.d/supervisord.conf
 
 exec "$@"
