e98ae096c54b3afed5c45bfdcbe03d12c4c55585
jason yuan <thisdk@gmail.com>
2025-04-16 15:39:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,15 @@
 : ${SIP:=SERVER}
 
 if [ "$SIP" = "SERVER" ]; then
+    sleep 3
+    IP_S=$(dig +short sing-box)
+    IP_W=$(dig +short wireguard)
     cp -f /etc/supervisor/conf.d/supervisord-server.conf.backup /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s#sing-box#$IP_S#g" /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s#wireguard#$IP_W#g" /etc/supervisor/conf.d/supervisord.conf
 else
     cp -f /etc/supervisor/conf.d/supervisord-client.conf.backup /etc/supervisor/conf.d/supervisord.conf
-    sed -i "s#SIP#$SIP#g" /etc/supervisor/conf.d/supervisord.conf
+    sed -i "s#SERVER_IP#$SIP#g" /etc/supervisor/conf.d/supervisord.conf
 fi
 
 exec "$@"
