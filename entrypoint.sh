006449aa51dc2a84614cda63733431fdbdfee523
jason yuan <thisdk@gmail.com>
2025-04-25 21:52:18 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,22 @@
 #!/bin/sh
+set -e
 
-: ${SERVER:=SERVER}
+: "${SERVER:=SERVER}"
+: "${MODE:=faketcp}"
 
 cp -f /etc/supervisor/conf.d/supervisord.conf.backup /etc/supervisor/conf.d/supervisord.conf
 
 if [ "$SERVER" = "SERVER" ]; then
+    echo "Waiting for DNS resolution..."
     sleep 3
-    SERVER_IP=$(dig +short sing-box)
-    sed -i "s#sing-box#$SERVER_IP#g" /etc/supervisor/conf.d/supervisord.conf
-else
-    sed -i "s#SERVER#$SERVER#g" /etc/supervisor/conf.d/supervisord.conf
+    SERVER=$(dig +short sing-box)
+    if [ -z "$SERVER" ]; then
+        echo "Failed to resolve sing-box address"
+        exit 1
+    fi
 fi
+
+sed -i "s#SERVER#$SERVER#g" /etc/supervisor/conf.d/supervisord.conf
+sed -i "s#MODE#$MODE#g" /etc/supervisor/conf.d/supervisord.conf
 
 exec "$@"
