5bd14c927504687b3ff5183e67ce4df4c2124d25
jason yuan <thisdk@gmail.com>
2025-05-11 13:26:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-: "${LISTENER:=0.0.0.0}"
+: "${LISTENER:=0.0.0.0:8585}"
 : "${SERVER:=SERVER}"
 : "${MODE:=faketcp}"
 
@@ -15,9 +15,9 @@ if [ "$SERVER" = "SERVER" ]; then
         echo "Failed to resolve sing-box address"
         exit 1
     fi
-    sed -i "s#LISTENER#$LISTENER#g" /etc/supervisor/conf.d/supervisord.conf
 fi
 
+sed -i "s#LISTENER#$LISTENER#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#SERVER#$SERVER#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#MODE#$MODE#g" /etc/supervisor/conf.d/supervisord.conf
 
