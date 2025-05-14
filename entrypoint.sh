a9f1de6853db2ac83239b84b13a7259ac7fa97f6
jason yuan <thisdk@gmail.com>
2025-05-14 19:03:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ fi
 RANDOM_PORT=$(shuf -i 10000-60000 -n 1)
 
 sed -i "s#LISTENER_ADDRESS#$LISTENER_ADDRESS#g" /etc/supervisor/conf.d/supervisord.conf
-sed -i "s#LISTENER_PORT#$LISTENER_PORT#g" /etc/supervisor/conf.d/supervisord.conf  
+sed -i "s#LISTENER_PORT#$LISTENER_PORT#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#TARGET_SERVER#$TARGET_SERVER#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#UDP2RAW_MODE#$UDP2RAW_MODE#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#INTERNAL_PORT#$RANDOM_PORT#g" /etc/supervisor/conf.d/supervisord.conf
