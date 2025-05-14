415db19971f5e4115dae4090da56911bbd8db7f3
jason yuan <thisdk@gmail.com>
2025-05-14 16:09:07 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -e
 
-: "${LISTENER:=0.0.0.0:8585}"
+: "${LISTENER:=0.0.0.0}"
+: "${PORT:=8585}"
 : "${SERVER:=SERVER}"
 : "${MODE:=faketcp}"
 
@@ -21,6 +22,7 @@ if [ "$SERVER" = "SERVER" ]; then
 fi
 
 sed -i "s#LISTENER#$LISTENER#g" /etc/supervisor/conf.d/supervisord.conf
+sed -i "s#PORT#$PORT#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#SERVER#$SERVER#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#RANDOM#$RANDOM#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#MODE#$MODE#g" /etc/supervisor/conf.d/supervisord.conf
