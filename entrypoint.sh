d614b90892101ef1b32a6a5c5454ebf9881c8ad3
jason yuan <thisdk@gmail.com>
2025-05-14 18:46:33 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,20 +6,14 @@ set -e
 : "${TARGET_SERVER:=SERVER}"
 : "${UDP2RAW_MODE:=faketcp}"
 
-cp -f /etc/supervisor/conf.d/supervisord.conf.backup /etc/supervisor/conf.d/supervisord.conf
-
 if [ "$TARGET_SERVER" = "SERVER" ]; then
-    echo "Waiting for DNS resolution..."
     sleep 3
     TARGET_SERVER=$(dig +short sing-box)
-    if [ -z "$TARGET_SERVER" ]; then
-        echo "Failed to resolve sing-box address"
-        exit 1
-    fi
 fi
 
 RANDOM_PORT=$(shuf -i 10000-60000 -n 1)
-echo "Generated random port for internal service: $RANDOM_PORT"
+
+cp -f /etc/supervisor/conf.d/supervisord.conf.backup /etc/supervisor/conf.d/supervisord.conf
 
 sed -i "s#LISTENER_ADDRESS#$LISTENER_ADDRESS#g" /etc/supervisor/conf.d/supervisord.conf
 sed -i "s#LISTENER_PORT#$LISTENER_PORT#g" /etc/supervisor/conf.d/supervisord.conf  
